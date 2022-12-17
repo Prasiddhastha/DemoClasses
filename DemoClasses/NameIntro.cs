10cca11f54d91dc9a6887b59bc5b8e77057f9101
Prasiddha Shrestha <prasiddhastha7@gmail.com>
2022-12-17 13:45:09 +0545

    Add project files.

---
+++ DemoClasses/NameIntro.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoClasses
+{
+    public class Person
+    {
+        public string firstName;
+        public string lastName;
+
+        public void Introduce()
+        {
+            Console.WriteLine($"Hello My name is {firstName} {lastName}");
+        }
+        //using static Modifier
+        public static void Introduc(string firstName, string lastName)
+        {
+            Console.WriteLine($"Hello My name is {firstName} {lastName}");
+        }
+    }
+}
---
+++ DemoClasses/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace DemoClasses
+{	
+	
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//creating object
+            var person = new Person();
+            person.firstName = "Prasiddha";
+            person.lastName = "Shrestha";
+
+			
+          A:  Console.Write("Enter option:");
+			int c =Convert.ToInt32( Console.ReadLine() );
+            
+            if (c > 2)
+            {
+                Console.WriteLine("wrong input");
+              
+                goto A;
+            }
+            switch (c)
+			{
+				case 1:
+                    person.Introduce();//accessing method creating object
+					break;
+
+				case 2:
+					Person.Introduc("Prasiddha","Shrestha");//accessing method directly from class
+					break;
+				
+
+            }
+			
+			
+		}
+	}
+}
